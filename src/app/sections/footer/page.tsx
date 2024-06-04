59c0b309815ffbf2c2a89d4af3dd8796b87dbaa6
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-06-03 19:24:58 -0700

    fixed links

--- src/app/sections/footer/page.tsx
+++ src/app/sections/footer/page.tsx
@@ -6,21 +6,25 @@ const ContactOptions = [
   {
     title: "Our Location",
     info: "4070 tuxedo drive, Victoria, BC, V8X 2L3",
+    link: "https://maps.app.goo.gl/cxiiPkJNfwJEYCtw6",
     icon: Home,
   },
   {
     title: "Make a Call",
-    info: "250-918-5591",
+    info: "(250)-918-5591",
+    link: "tel:(250)-918-5591",
     icon: Phone,
   },
   {
     title: "Send are Email",
     info: "info@albcanada.com",
+    link: "mailto:someone@example.com",
     icon: Mails,
   },
   {
     title: "Working Hours",
     info: "Everyday 8am - 6pm",
+    link: "https://maps.app.goo.gl/cxiiPkJNfwJEYCtw6",
     icon: DoorOpen,
   },
 ];
@@ -37,13 +41,18 @@ const Footer = () => {
 
           <div className="flex gap-8 justify-between flex-wrap mt-12">
             {ContactOptions.map((contact, i) => (
-              <div key={i} className="flex gap-4 flex-1 min-w-[300px]">
+              <a
+                key={i}
+                className="flex gap-4 flex-1 min-w-[300px]"
+                href={contact.link}
+                target="_blank"
+              >
                 <contact.icon className="box-content	text-secondary min-w-8 min-h-8 p-5 bg-primary" />
                 <div className="text-white">
                   <h1 className="teko text-2xl">{contact.title}</h1>
                   <p className="serrat">{contact.info}</p>
                 </div>
-              </div>
+              </a>
             ))}
           </div>
         </Container>
